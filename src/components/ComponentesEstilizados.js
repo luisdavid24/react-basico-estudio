ce25cfc59f0bae9f6660a4e63164693a170c3792
Luis david <david12.29@hotmail.com>
2022-03-16 19:22:17 -0500

    dejemos este proyecto hasta aqui

--- src/components/ComponentesEstilizados.js
+++ src/components/ComponentesEstilizados.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled,{css} from "styled-components";
+import styled,{css,keyframes} from "styled-components";
 
 export default function ComponentesEstilizados(){
     let mainColor="#db7093",
@@ -7,13 +7,23 @@ export default function ComponentesEstilizados(){
 
     const setTransitionTime= (time)=>`all${time}esea-in-out`;
     
+    const fadeln=keyframes`
+        0%{
+            opacity:0;
+        }
+        100%{
+            opacity:1;
+        }
+    `;
+
     const MyH3=styled.h3`
         padding:2rem;
         text-align:center;
         color:${props=>props.color};
         color:${({color}) => color||"#000"};
         background-color:${mainColor};
-        
+        animation:${fadeln} 2s ease-out;
+
         ${(props)=>props.isButton && css`
             margin:auto;
             max-width:50%;
@@ -26,6 +36,7 @@ export default function ComponentesEstilizados(){
         &:hover {
             background-color:${mainAlphaColor80};
         }
+
     `;
     
     return(
@@ -34,7 +45,7 @@ export default function ComponentesEstilizados(){
             <MyH3>hola soy un h3 estilizado con styled-components</MyH3>
             <MyH3 color="#61dafb">hola soy un h3 estilizado con styled-components</MyH3>
             <MyH3 isButton>hola soy un h3 estilizado como boton</MyH3>
-        
+
         </>
     )
 }
